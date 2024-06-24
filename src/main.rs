2a76502f28d532293c9b52051883368af9b2c0c3
Braden Everson <bradeneverson@gmail.com>
2024-06-24 10:20:09 -0500

    Pathfinding and storing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use genezip::huffman::tree::HuffmanTree;
+
 fn main() {
-    println!("Hello, world!");
+    let text = "ATGCCCGTA".chars().collect::<Vec<char>>();
+    let tree = HuffmanTree::from_data(&text);
+
+    for elem in text {
+        println!("How to get to {} in tree: \n{:?}", elem, tree.encodings_to(&elem));
+    }
 }
